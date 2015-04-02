b541bf7b43229206f196419ca1bb65ac00836274
carlad <carlad@users.noreply.github.com>
2015-04-02 13:36:33 +0200

    add url as argument for setup

--- spec/topaz_spec.rb
+++ spec/topaz_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 RSpec::Mocks.configuration.syntax = :should
 
 describe Travis::Topaz do
+  let(:url) { 'http://test_url.com' }
   before do
-    Travis::Topaz.setup
+    Travis::Topaz.setup(url)
   end
 
   describe '#update' do
@@ -12,7 +13,7 @@ describe Travis::Topaz do
     context 'Queue has less than 100 waiting updates' do
       it 'sends post request to topaz app' do
         Travis::Topaz.update(event_data)
-        expect(a_request(:post, "https://travis-pro-topaz-staging.herokuapp.com/event/new")).to have_been_made.once
+        expect(a_request(:post, "http://test_url.com/event/new")).to have_been_made.once
       end
     end
 
