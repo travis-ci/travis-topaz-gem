c8d7d4b4c3902069d2e6b54d7dd18c6823b7a0f3
carlad <carla@travis-ci.com>
2015-04-02 18:19:09 +0200

    added some sleep to the specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,5 @@ require 'webmock/rspec'
 require 'faraday'
 require 'travis/topaz'
 require 'travis/support/logger'
+
+RSpec::Mocks.configuration.syntax = :should
--- spec/topaz_spec.rb
+++ spec/topaz_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-RSpec::Mocks.configuration.syntax = :should
-
 describe Travis::Topaz do
   let(:url) { 'http://test.com' }
   before do
@@ -9,22 +7,27 @@ describe Travis::Topaz do
   end
 
   describe '#update' do
+
     let(:event) { { sample: "event" } }
-    context 'Queue has less than 100 waiting updates' do
+
+    describe 'Queue has less than 100 waiting updates' do
       it 'sends post request to topaz app' do
         Travis::Topaz.update(event)
+        sleep 0.1
         expect(a_request(:post, "http://test.com/event/new")).to have_been_made.once
       end
     end
 
-    context 'Queue has 100 or more waiting updates' do
+    describe 'Queue has 100 or more waiting updates' do
       before do
         Travis::Topaz.queue.stub(:num_waiting).and_return(101)
       end
       it 'does not post request topaz app' do
         Travis::Topaz.update(event)
+        sleep 0.1
         expect(a_request(:post, "https://test.com/event/new")).not_to have_been_made
       end
     end
+
   end
 end
